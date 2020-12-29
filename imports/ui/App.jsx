9c436f7841c8634a773cc707e11bab9d220ad9ea
Jorge Luis Sánchez Labastida <jsanchez.labastida@gmail.com>
2020-12-28 23:47:39 -0600

    Step 1.1 to 1.5 work fine

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
+import { Task } from './Task.jsx';
+
+const tasks = [
+  {_id:1, text:'T1'},
+  {_id:2, text:'T2'},
+  {_id:3, text:'T3'},
+  {_id:4, text:'T4'}
+]
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
+    <ul>
+      { tasks.map(task => <Task key={task.key} task={task} />) }
+    </ul>
     <Hello/>
     <Info/>
   </div>
