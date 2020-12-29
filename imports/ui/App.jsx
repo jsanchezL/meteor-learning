7480b5d218657e4aea5630c97e4f6f1ef7d985a1
Jorge Luis Sánchez Labastida <jsanchez.labastida@gmail.com>
2020-12-29 00:08:09 -0600

    Steps 2.1 to 2.3, work with MongoDB, React Hook

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
-import { Task } from './Task.jsx';
+import { useTracker } from 'meteor/react-meteor-data'
+import { TasksCollection } from '/imports/api/TasksCollection'
+import { Task } from './Task.jsx'
 
-const tasks = [
-  {_id:1, text:'T1'},
-  {_id:2, text:'T2'},
-  {_id:3, text:'T3'},
-  {_id:4, text:'T4'}
-]
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <ul>
-      { tasks.map(task => <Task key={task.key} task={task} />) }
-    </ul>
-    <Hello/>
-    <Info/>
-  </div>
-);
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch())
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <ul>
+        {tasks.map(task => <Task key={task.key} task={task} />)}
+      </ul>
+    </div>
+  )
+}
--- server/main.js
+++ server/main.js
@@ -1,31 +1,45 @@
 import { Meteor } from 'meteor/meteor';
 import { LinksCollection } from '/imports/api/links';
+import { TasksCollection } from '/imports/api/TasksCollection'
 
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
+const insertTask = taskText => TasksCollection.insert({text: taskText})
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
-
-    insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
-    });
+  if(TasksCollection.find().count() === 0){
+    [
+      'T1',
+      'T2',
+      'T3',
+      'T4'
+    ].forEach(insertTask);
   }
-});
+})
+
+// function insertLink({ title, url }) {
+//   LinksCollection.insert({title, url, createdAt: new Date()});
+// }
+
+// Meteor.startup(() => {
+//   // If the Links collection is empty, add some data.
+//   if (LinksCollection.find().count() === 0) {
+//     insertLink({
+//       title: 'Do the Tutorial',
+//       url: 'https://www.meteor.com/tutorials/react/creating-an-app'
+//     });
+
+//     insertLink({
+//       title: 'Follow the Guide',
+//       url: 'http://guide.meteor.com'
+//     });
+
+//     insertLink({
+//       title: 'Read the Docs',
+//       url: 'https://docs.meteor.com'
+//     });
+
+//     insertLink({
+//       title: 'Discussions',
+//       url: 'https://forums.meteor.com'
+//     });
+//   }
+// });
