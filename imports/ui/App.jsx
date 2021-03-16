36106ce101af39eb1884f3b862d241a000b22df3
Jorge Luis Sánchez Labastida <jsanchez.labastida@gmail.com>
2021-03-15 21:56:23 -0600

    Avanzado hasta el 6

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,16 +2,41 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data'
 import { TasksCollection } from '/imports/api/TasksCollection'
 import { Task } from './Task.jsx'
-
+import { TaskForm } from './TaskForm'
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch())
+  const tasks = useTracker(() => TasksCollection.find({},{ sort: { createdAt: -1 }}).fetch())
+
+  const toggleChecked = ({ _id, isChecked }) => {
+    TasksCollection.update(_id,{ $set: {isChecked: !isChecked}})
+  }
+
+  const deleteTask = ({_id}) => TasksCollection.remove(_id)
+
   return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
-      <ul>
-        {tasks.map(task => <Task key={task.key} task={task} />)}
-      </ul>
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>? To Do List</h1>
+          </div>
+        </div>
+      </header>
+
+      <div className="main">
+        <TaskForm />
+
+        <ul className="tasks">
+          {tasks.map(task => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
--- server/main.js
+++ server/main.js
@@ -1,5 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
 import { TasksCollection } from '/imports/api/TasksCollection'
 
 const insertTask = taskText => TasksCollection.insert({text: taskText})
@@ -14,32 +13,3 @@ Meteor.startup(() => {
     ].forEach(insertTask);
   }
 })
-
-// function insertLink({ title, url }) {
-//   LinksCollection.insert({title, url, createdAt: new Date()});
-// }
-
-// Meteor.startup(() => {
-//   // If the Links collection is empty, add some data.
-//   if (LinksCollection.find().count() === 0) {
-//     insertLink({
-//       title: 'Do the Tutorial',
-//       url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-//     });
-
-//     insertLink({
-//       title: 'Follow the Guide',
-//       url: 'http://guide.meteor.com'
-//     });
-
-//     insertLink({
-//       title: 'Read the Docs',
-//       url: 'https://docs.meteor.com'
-//     });
-
-//     insertLink({
-//       title: 'Discussions',
-//       url: 'https://forums.meteor.com'
-//     });
-//   }
-// });
