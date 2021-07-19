e552b3f9d51d61e7a70f701c7ae1f0f1275b6d96
Jorge Luis Sánchez Labastida <jsanchez.labastida@gmail.com>
2021-07-19 17:27:24 -0500

    Sección 6

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import { useTracker } from 'meteor/react-meteor-data'
-import { TasksCollection } from '/imports/api/TasksCollection'
-import { Task } from './Task.jsx'
-import { TaskForm } from './TaskForm'
+import React, {useState} from 'react'
+import {useTracker} from 'meteor/react-meteor-data'
+import {TasksCollection} from '/imports/api/TasksCollection'
+import {Task} from './Task.jsx'
+import {TaskForm} from './TaskForm'
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({},{ sort: { createdAt: -1 }}).fetch())
+  const [hideCompleted, setHideCompleted] = useState (false)
+  const hideCompletedFilter = { isChecked: { $ne: true } }
+  
+  const tasks = useTracker (() =>
+    TasksCollection.find (hideCompleted ? hideCompletedFilter : {}, {sort: {createdAt: -1}}).fetch ()
+  )
 
-  const toggleChecked = ({ _id, isChecked }) => {
-    TasksCollection.update(_id,{ $set: {isChecked: !isChecked}})
+  const toggleChecked = ({_id, isChecked}) => {
+    TasksCollection.update (_id, {$set: {isChecked: !isChecked}})
   }
 
-  const deleteTask = ({_id}) => TasksCollection.remove(_id)
+  const deleteTask = ({_id}) => TasksCollection.remove (_id)
+
+  const pendingTasksCount = useTracker(() =>
+    TasksCollection.find(hideCompletedFilter).count()
+  )
+
+  const pendingTasksTitle = `${
+    pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`
 
   return (
     <div className="app">
       <header>
         <div className="app-bar">
           <div className="app-header">
-            <h1>? To Do List</h1>
+            <h1>? To Do List {pendingTasksTitle} </h1>
           </div>
         </div>
       </header>
@@ -26,8 +39,14 @@ export const App = () => {
       <div className="main">
         <TaskForm />
 
+        <div className="filter">
+          <button onClick={() => setHideCompleted (!hideCompleted)}>
+            {hideCompleted ? 'Show All' : 'Hide Completed'}
+          </button>
+        </div>
+
         <ul className="tasks">
-          {tasks.map(task => (
+          {tasks.map (task => (
             <Task
               key={task._id}
               task={task}
